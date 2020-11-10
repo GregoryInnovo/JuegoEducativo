2a7264bfda56c936301131a1800cae1246303607
Gregory Innovo <gregoryinnovo@gmail.com>
2020-11-10 18:54:19 -0500

    Se agrego un spawner de food

---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    public List<GameObject> food = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float num = 1f;
+        InstantiateFood(num);
+    }
+
+    public void InstantiateFood(float num)
+    {
+        if (num == 1f)
+        {
+            Instantiate(food[0], new Vector2(Random.Range(0.0f, 0.0f), Random.Range(0.0f, 20.0f)), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/collectFood.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class collectFood : MonoBehaviour
+{
+
+    public Text puntaje;
+
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puntaje.text = "Puntaje: " + score;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        score++;
+    }
+}
