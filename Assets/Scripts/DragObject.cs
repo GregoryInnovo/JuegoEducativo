e64239cf11d20e9c4c663199d3ac7d3cabae6190
Gregory Innovo <gregoryinnovo@gmail.com>
2020-11-14 16:43:48 -0500

    Se agrego la funcion para arrastar un obj 2d

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObject : MonoBehaviour, IDragHandler
+{
+    public float z = 0.0f;
+    public void OnDrag(PointerEventData data)
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = z;
+
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+
+}
---
+++ Assets/Scripts/DragUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragUI : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData data)
+    {
+        transform.position = data.position;
+    }
+
+}
